8d83915fdd2368106a3ab02afbff79db2cec85c2
Myles Watson <mylesgw@google.com>
2017-03-31 11:25:04 -0700

    jf: Add libbt-vendor to device.mk
    Bug: 36810308
    Test: build
    Change-Id: Iae4a6340a184dde818603b546470c8cf883f2f7b

--- jf-common.mk
+++ jf-common.mk
@@ -87,8 +87,8 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl
-
+    android.hardware.bluetooth@1.0-impl \
+    libbt-vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/bluetooth/bcm4335_prepatch.hcd:system/vendor/firmware/bcm4335_prepatch.hcd
