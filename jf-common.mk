6435057ed0f10731d6028b7a028caf7acc1bb1b7
Kevin F. Haggerty <haggertk@lineageos.org>
2017-08-13 15:55:58 -0600

    jf: Use the OSS GPS HAL
    * Use bacon's GPS blob stack
    * Build the package
    * Drop our custom gps.h
    
    Change-Id: Ib781b06310a4b11a5e69fe759549a89ce65fb1c8

--- jf-common.mk
+++ jf-common.mk
@@ -104,6 +104,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     SamsungDoze
 
+# GPS
+PRODUCT_PACKAGES += \
+    gps.msm8960
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:/system/etc/gps.conf \
     $(LOCAL_PATH)/configs/sap.conf:/system/etc/sap.conf
