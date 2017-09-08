bffa7626be5b8b925b67380d63cdf4e8ab74ca50
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-09-09 01:33:29 +0200

    jf: nfc: Rename nfc_nci.bcm2079x from .msm8960 to .default
    Change-Id: I7a271fee2cb73abb4c17493c972317bbcd83fdcf
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- jf-common.mk
+++ jf-common.mk
@@ -174,7 +174,7 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-impl-bcm \
     libnfc-nci \
     libnfc_nci_jni \
-    nfc_nci.bcm2079x.msm8960 \
+    nfc_nci.bcm2079x.default \
     NfcNci \
     Tag \
     com.android.nfc_extras
