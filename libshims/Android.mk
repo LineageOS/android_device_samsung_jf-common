e104fa22e9453f766e6e2d9beeccf7648dae334c
Nolen Johnson <johnsonnolen@gmail.com>
2018-01-29 01:11:26 -0500

    jf: ril: Drop current RIL setup
    * We are migrating to an OSS RIL as is basically required by AOSP 8.1
      unless you have 8.0+ RIL blobs.
    
    Change-Id: If81bb4f29336132571a4c905b7e57f48be16efda

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -14,17 +14,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-    samsung_ril.cpp
-
-LOCAL_SHARED_LIBRARIES := libbinder
-
-LOCAL_MODULE := libshim_ril
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
