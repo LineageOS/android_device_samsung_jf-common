e3c3fc7e550a6d45edfad8cd6f09de1dfcfaccb7
Paras Nagda <pnagda@codeaurora.org>
2019-05-06 15:37:56 +0530

    jf-common: Add mediaswcodec to /vendor/etc/seccomp_policy
    Change-Id: If2075d8b78348c28f525e87aa01bac0537d9b397

--- jf-common.mk
+++ jf-common.mk
@@ -244,7 +244,8 @@ PRODUCT_PACKAGES += \
 # Seccomp Policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy \
+    $(LOCAL_PATH)/seccomp_policy/mediaswcodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaswcodec.policy
 
 # Sensors
 PRODUCT_PACKAGES += \
