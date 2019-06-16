d9b19be6f6d0d2dc3a1447e1103ce30db7389109
Nolen Johnson <johnsonnolen@gmail.com>
2019-06-16 07:16:36 +0000

    jf-common: Remove outdated dalvik configs
    Change-Id: I83952d44dc809b9e14b071a8e00add9cb43e303e

--- jf-common.mk
+++ jf-common.mk
@@ -55,8 +55,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
