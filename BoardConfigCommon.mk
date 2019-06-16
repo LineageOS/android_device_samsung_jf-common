67c31b3f7052c52a4934de4e994443a5646e8413
Arne Coucheron <arco68@gmail.com>
2019-06-16 05:50:37 +0000

    jf-common: charger: Remove {deprecated|unused} flags
    Change-Id: I325201c5bea761af2f640006104f0793b6b18577
    Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -86,8 +86,6 @@ TARGET_PROCESS_SDK_VERSION_OVERRIDE += \
     /system/vendor/bin/mm-qcamera-daemon=22
 
 # Charger
-BOARD_CHARGING_CMDLINE_NAME := "androidboot.bootchg"
-BOARD_CHARGING_CMDLINE_VALUE := "true"
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
 # Display
