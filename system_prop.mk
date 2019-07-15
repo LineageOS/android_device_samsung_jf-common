9ea212cef98b116ec3f5de11688959c8f76b5c9e
Yifan Hong <elsk@google.com>
2019-07-15 16:43:26 -0700

    jf-common: Replace BOARD_CHARGER_ENABLE_SUSPEND with ro.charger.enable_suspend
    Test: charger mode
    Bug: 124118169
    Change-Id: I3825d08e38d677bd7154879609857de302348b6f

--- system_prop.mk
+++ system_prop.mk
@@ -64,3 +64,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.lmk.kill_timeout_ms=100 \
     ro.lmk.log_stats=true \
     ro.lmk.use_minfree_levels=true
+
+# Charger
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.charger.enable_suspend=true
