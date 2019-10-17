09b71a9486fd26cf9cd160673d146a12a4ffe40a
Arne Coucheron <arco68@gmail.com>
2019-10-17 10:06:05 +0200

    jf-common: Copy init.qcom.recovery.rc to recovery/root, not root
    Change-Id: Id7b2a90f714462768a4010eca1c09f63172124b5

--- jf-common.mk
+++ jf-common.mk
@@ -221,7 +221,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.qcom:root/fstab.qcom \
     $(LOCAL_PATH)/rootdir/init.qcom.power.rc:root/init.qcom.power.rc \
-    $(LOCAL_PATH)/rootdir/init.qcom.power.rc:root/init.recovery.qcom.rc \
+    $(LOCAL_PATH)/rootdir/init.qcom.power.rc:recovery/root/init.recovery.qcom.rc \
     $(LOCAL_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
     $(LOCAL_PATH)/rootdir/init.qcom.usb.rc:root/init.qcom.usb.rc \
     $(LOCAL_PATH)/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc
