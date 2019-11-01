587e79144aedba39581a86b48cab952fd1cd3e50
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-01 07:30:01 -0500

    jf-common: remove overlay-lineage/
    * There is no need for a separate overlay-lineage on a device
      with no upstream
    
    Change-Id: Id439d0d05a370a8c29a2b052f7e98f7678d29349

--- jf-common.mk
+++ jf-common.mk
@@ -25,7 +25,6 @@ $(call inherit-product-if-exists, vendor/samsung/jf-common/jf-common-vendor.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
