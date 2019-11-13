5c8c1b8e9ec738faaa9d164e7dc80a5b65ae7831
Wang Han <416810799@qq.com>
2019-11-13 16:23:50 +0800

    jf-common: Unset TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS
     * This is only used in system sepolicy to ignore some neverallows,
       but as we ignore all neverallows in qcom sepolicy, it is useless.
    
    Change-Id: Ic6e58a6ba68aaf6b5bbe59c7206b3fedfd886248

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -123,9 +123,6 @@ DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 # DexPreopt debug info
 WITH_DEXPREOPT_DEBUG_INFO := false
 
-# Legacy
-TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
-
 # Filesystem
 BOARD_ROOT_EXTRA_FOLDERS := efs firmware firmware-mdm
 BOARD_ROOT_EXTRA_SYMLINKS := /data/tombstones:/tombstones
