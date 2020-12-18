3e9134ee68ce4d089f2bbc18da07e5929c6ed663
Arne Coucheron <arco68@gmail.com>
2020-12-18 11:52:44 +0100

    jf-common: Set custom dalvik heap values
    Same as stock ROM.
    
    Change-Id: Ied94bd53650d42f3bfd1f2f1c4b05b770c5f29de

--- system_prop.mk
+++ system_prop.mk
@@ -62,3 +62,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Charger
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.charger.enable_suspend=true
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=128m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapmaxfree=8m
