d634cf4aaed8262e508a56980f7d3daa20eb6420
Arne Coucheron <arco68@gmail.com>
2021-01-11 04:22:34 +0100

    jf-common: Don't build FlipFlap for jactivelte
    Change-Id: I6823d523a5730226a61b942def9d88718ae754b4

--- jf-common.mk
+++ jf-common.mk
@@ -138,9 +138,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     resize2fs_static
 
+ifeq ($(findstring jactivelte,$(TARGET_PRODUCT)),)
 # FlipFlap
 PRODUCT_PACKAGES += \
     FlipFlap
+endif
 
 # GPS
 PRODUCT_PACKAGES += \
