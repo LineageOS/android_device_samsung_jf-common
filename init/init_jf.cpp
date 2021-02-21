dac1cd400df5706574b825e9ddf311f41215bf7c
Nolen Johnson <johnsonnolen@gmail.com>
2021-02-21 17:09:28 -0500

    jf-common: init: Check for null serial number before overwriting
    * Some variants (looking at you, jfltevzw), read out `00000000000`
      from `/efs/FactoryApp/serial_no`. This results in `ro.serialno`
      being overwriten from the correct value kernel cmdline passes
      (androidboot.serialno) on these devices.
    
    * Therefore, add a check to see if it reads out `00000000000`, and
      if it does, assume a fallback to leaving `ro.serialno` as
      `androidboot.serialno` output.
    
    * Bonus points, convert the `const char` to and `int` to check for
      users who many have just zeroed out the file themselve accidentally.
    
    Change-Id: I4741808577b6d548e380ba807688dfcccfb4e338
    (cherry picked from commit 728f2483d5181e570550b42d3d75aca6a169559c)

--- init/init_jf.cpp
+++ init/init_jf.cpp
@@ -49,7 +49,11 @@ void vendor_load_properties()
 
     if (ReadFileToString(serial_number_file, &serial_number)) {
         serial_number = Trim(serial_number);
+        int serial_check;
+        serial_check = atoi(serial_number.c_str());
+        if (serial_check != 0) {
         property_override("ro.serialno", serial_number.c_str());
+        }
     }
 
     const auto set_ro_product_prop = [](const std::string &source,
