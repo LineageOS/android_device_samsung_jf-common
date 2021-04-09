a64d975820a430b5a22faa14bd171745d86c4ac6
Kevin F. Haggerty <haggertk@lineageos.org>
2021-04-09 08:18:37 -0600

    jf-common: Don't let builds complete without vendor tree
    * There is zero reason to ever build without blobs. We've even seen
      this situation with official builds from our servers. It's always
      better for a build to fail than it is for it to produce something
      that has no chance at working.
    
    Change-Id: I4968795670c91f691e9ecdc0e4af62e16ba3a93a

--- jf-common.mk
+++ jf-common.mk
@@ -21,7 +21,7 @@ $(LOCAL_PATH) := device/samsung/jf-common
 $(call inherit-product, vendor/lineage/build/target/product/product_launched_with_j_mr2.mk)
 
 # Get non-open-source specific aspects if available
-$(call inherit-product-if-exists, vendor/samsung/jf-common/jf-common-vendor.mk)
+$(call inherit-product, vendor/samsung/jf-common/jf-common-vendor.mk)
 
 # Soong
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
