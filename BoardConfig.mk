3a0d381c08810c31568bc240a1183d0f1a0788cb
Kevin F. Haggerty <haggertk@lineageos.org>
2021-04-09 08:18:37 -0600

    jf-common: Don't let builds complete without vendor tree
    * There is zero reason to ever build without blobs. We've even seen
      this situation with official builds from our servers. It's always
      better for a build to fail than it is for it to produce something
      that has no chance at working.
    
    Change-Id: I4968795670c91f691e9ecdc0e4af62e16ba3a93a

---
+++ BoardConfig.mk
@@ -0,0 +1,37 @@
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2021 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# inherit from common klte
+include device/samsung/klte-common/BoardConfigCommon.mk
+
+TARGET_OTA_ASSERT_DEVICE := klte,kltekor,kltektt,kltelgt,klteskt
+
+# Kernel
+TARGET_KERNEL_CONFIG := lineage_kltekor_defconfig
+
+# Fingerprint
+include $(COMMON_PATH)/fingerprint/board.mk
+
+# Init
+TARGET_INIT_VENDOR_LIB := libinit_kltekor
+
+# NFC
+include $(COMMON_PATH)/nfc/pn547/board.mk
+
+# Radio/RIL
+include $(COMMON_PATH)/radio/single/board.mk
+
+# inherit from the proprietary version
+include vendor/samsung/kltekor/BoardConfigVendor.mk
---
+++ device.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2017-2021 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/samsung/kltekor/kltekor-vendor.mk)
+$(call inherit-product, vendor/samsung/klte-common/klte-common-vendor-ril-m.mk)
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Fingerprint
+$(call inherit-product, device/samsung/klte-common/fingerprint/product.mk)
+
+# NFC
+$(call inherit-product, device/samsung/klte-common/nfc/pn547/product.mk)
+
+# common klte
+$(call inherit-product, device/samsung/klte-common/klte.mk)
